36f62b60744c636571b2437bc03cf36fe9cd0afd
gparas <parageos@gmail.com>
2020-10-27 23:10:31 +0200

    authenticating user

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const movies = require('./routes/movies');
 const customers = require('./routes/customers');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 const app = express();
 
 const uri =
@@ -22,6 +24,8 @@ app.use(express.json());
 app.use('/api/genres/', genres);
 app.use('/api/movies/', movies);
 app.use('/api/customers/', customers);
+app.use('/api/users/', users);
+app.use('/api/auth/', auth);
 
 const port = process.env.PORT || 3000;
 
---
+++ models/user.js
@@ -0,0 +1,33 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+mongoose.set('useCreateIndex', true);
+
+// Schemas
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 5, maxlength: 50 },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  password: { type: String, required: true, minlength: 5, maxlength: 1024 },
+});
+
+const User = mongoose.model('User', userSchema);
+
+const validateUser = (user) => {
+  const schema = Joi.object({
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(255).email().required(),
+    password: Joi.string().min(5).max(255).required(),
+  });
+  return schema.validate(user);
+};
+
+module.exports = {
+  User,
+  validateUser,
+};
