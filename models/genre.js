d72bdb96e1fa068a9c41f225c75a0a1bf0e89e20
gparas <parageos@gmail.com>
2020-10-27 09:33:07 +0200

    restructuring

--- models/genre.js
+++ models/genre.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const mongoose = require('mongoose');
 
 // Schemas
@@ -5,5 +6,16 @@ const genreSchema = new mongoose.Schema({
   name: { type: String, required: true, minlength: 3 },
 });
 
-// Models
-module.exports = mongoose.model('Genre', genreSchema);
+const Genre = mongoose.model('Genre', genreSchema);
+
+const validateGenre = (genre) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(genre);
+};
+
+module.exports = {
+  Genre,
+  validateGenre,
+};
--- routes/customers.js
+++ routes/customers.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Customer = require('../models/customer');
-const U = require('./utils');
+const { Customer, validateCustomer } = require('../models/customer');
 
 router.get('/', async (req, res) => {
   const customers = await Customer.find().sort('name');
@@ -17,7 +16,7 @@ router.get('/:id', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { error } = U.validateCustomer(req.body);
+  const { error } = validateCustomer(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let customer = new Customer({
@@ -31,7 +30,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = U.validateCustomer(req.body);
+  const { error } = validateCustomer(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   const customer = await Customer.findByIdAndUpdate(req.params.id, {
